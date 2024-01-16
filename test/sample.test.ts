75cc206ab0d082b4acb497e6beb16d2bed27f432
Jamie Holmes <jamieholmes@Jamies-MacBook-Air.local>
2024-01-16 19:25:26 +0000

    Tests not working

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -2,21 +2,21 @@ import { expect, test, vi } from 'vitest'
 import { createUser, } from '~/models/user.server'
 import { editFinancialDetails } from '~/models/financial.server'
 import prisma from 'lib/__mocks__/prisma'
+import bcrypt from "bcryptjs";
 
  vi.mock('../lib/prisma')
 
-test('Testing creating a new user',async () => {
-    const newUser = {email : 'user@prisma.com', password : 'Prisma123'}
-    const { email, password } = newUser
+// test('Testing creating a new user',async () => {
+//     const newUser = {email : 'user@prisma.com', password : 'Prisma123'}
+//     const { email, password } = newUser
 
-    prisma.user.create.mockResolvedValue({...newUser, id: '1'})
-    const user = await createUser(email, password)
+//     prisma.user.create.mockResolvedValue({...newUser, id: '1'})
+//     const user = await createUser(email, password)
 
-    expect(user).toStrictEqual({ ...newUser, id: 1, });
-})
+//     expect(user).toStrictEqual({ ...newUser, id: 1, });
+// })
 
-test('Testing creating a new user',async () => {
-
+//test('Testing creating a new user',async () => {
     // const newUser = {email : 'user@prisma.com', password : 'Prisma123'}
     // const { email, password } = newUser
 
@@ -24,29 +24,29 @@ test('Testing creating a new user',async () => {
     // const user = await createUser(email, password)
 
     // expect(user).toStrictEqual({ ...newUser, id: 1, });
-})
+//})
 
 
-// test('Testing adding financial details', async () => {
-//     const financialDetailsData = {
-//       balance: 3333,
-//       income: 3333,
-//       savings: 3333,
-//       userId: "clre2urum0000r426y91gdva0",
-//     };
+test('Testing adding financial details', async () => {
+    const financialDetailsData = {
+      balance: 3333,
+      income: 3333,
+      savings: 3333,
+      userId: "clre2urum0000r426y91gdva0",
+    };
 
-//     prisma.financialDetails.create.mockResolvedValue({...})
+    prisma.financialDetails.create.mockResolvedValue({...})
   
-//     const financialDetails = await editFinancialDetails(financialDetailsData);
+    const financialDetails = await editFinancialDetails(financialDetailsData);
   
-//     expect(financialDetails).toStrictEqual({
-//         balance: 3333,
-//         income: 3333,
-//         savings: 3333,
-//         userId: "clre2urum0000r426y91gdva0",
-//         id: expect.any(String), // Assuming the id returned is a string
-//         createdAt: "2024-01-14T22:38:26.137Z"
-//     });
-// });
+    expect(financialDetails).toStrictEqual({
+        balance: 3333,
+        income: 3333,
+        savings: 3333,
+        userId: "clre2urum0000r426y91gdva0",
+        id: expect.any(String), // Assuming the id returned is a string
+        createdAt: "2024-01-14T22:38:26.137Z"
+    });
+});
 
 
