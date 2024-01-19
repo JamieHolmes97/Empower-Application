14fb8edfe1833e76184bcce3a48321d86cb46251
Jamie Holmes <jamieholmes@Jamies-MacBook-Air.local>
2024-01-19 15:04:53 +0000

    Testing index routing

---
+++ app/routes/dashboard.createbudget.tsx
@@ -0,0 +1,13 @@
+import NavBar from "~/components/NavBar"
+
+export default function CreateBudget() {
+    return (
+        <div>
+            <h1>
+                Testing this route
+            </h1>
+        </div>
+    )
+
+
+}
