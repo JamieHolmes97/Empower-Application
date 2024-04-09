624cfa224620ae318a9396dee1ccf7f0a484d9d6
Jamie Holmes <jamieholmes@Jamies-MacBook-Air.local>
2024-04-09 15:20:08 +0100

    Changes to tips page to display all tips form user and community

--- app/components/TipsDetailsCard.jsx
+++ app/components/TipsDetailsCard.jsx
@@ -1,15 +1,7 @@
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { styled } from "@mui/material/styles";
-import AddIcon from "@mui/icons-material/Add";
-import { Link } from "@remix-run/react";
+import { ThemeProvider, createTheme, styled } from "@mui/material/styles";
+import { useState } from "react";
+import { Box, Card, CardContent, Divider, Grid, Typography, Button } from "@mui/material";
 
 const darkTheme = createTheme({
   palette: {
@@ -21,14 +13,36 @@ const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: "center",
+  textAlign: "left",
   color: theme.palette.text.secondary,
 }));
 
- const TipsDetailsCard = ({ tipsData, userTipsData }) => {
+const TipsDetailsCard = ({ tipsData, userTipsData }) => {
+  const [showAllTips, setShowAllTips] = useState(false);
+  const [showAllUserTips, setShowAllUserTips] = useState(false);
+
+  // Define the number of tips to display initially
+  const initialTipsCount = 8;
+  const initialUserTipsCount = 4;
+
+  // Calculate the number of rows to display initially
+  const initialRowCount = Math.ceil(initialTipsCount / 4);
+
+  // Slice the array to display only the first 4/5 rows initially
+  const displayedTips = showAllTips ? tipsData : tipsData.slice(0, initialTipsCount);
+  const displayedUserTips = showAllTips ? userTipsData : userTipsData.slice(0, initialUserTipsCount);
+
+  // Function to handle button click to toggle between showing all tips and showing only the first 4/5 rows
+  const handleToggleTips = () => {
+    setShowAllTips(!showAllTips);
+  };
+  const handleToggleUserTips = () => {
+    setShowAllTips(!showAllUserTips);
+  };
+
   return (
     <ThemeProvider theme={darkTheme}>
-      <Box sx={{ minWidth: 275 }}>
+      <Box sx={{ minWidth: 275 }} className="mb-4">
         <Card variant="outlined">
           <CardContent>
             <Box
@@ -37,14 +51,41 @@ const Item = styled(Paper)(({ theme }) => ({
                 justifyContent: "space-between",
               }}
             >
-                <Typography
-                        sx={{ fontSize: 14 }}
-                        color="text.secondary"
-                        gutterBottom
-                      >
-                        Your Tips
-                      </Typography>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Community Tips
+              </Typography>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Total Tips: {tipsData.length}
+              </Typography>
             </Box>
+            <Box sx={{ flexGrow: 1 }}>
+              <Grid container spacing={2}>
+                {displayedTips.map((tipsData, tipIndex) => (
+                  <Grid item xs={3} key={tipIndex}>
+                    <div className="mb-4">
+                      <Item>
+                        <Typography variant="subtitle1" component="div">
+                          {tipsData.university}
+                        </Typography>
+                        <Divider />
+                        <Typography variant="subtitle1" component="div">
+                          {tipsData.message}
+                        </Typography>
+                        <Divider />
+                        <Typography variant="subtitle1" component="div">
+                          {tipsData.name}
+                        </Typography>
+                      </Item>
+                    </div>
+                  </Grid>
+                ))}
+              </Grid>
+            </Box>
+            {!showAllTips && (
+              <Button onClick={handleToggleTips} variant="outlined" color="primary" sx={{ mt: 2 }}>
+                Show More Tips
+              </Button>
+            )}
           </CardContent>
         </Card>
       </Box>
@@ -57,15 +98,43 @@ const Item = styled(Paper)(({ theme }) => ({
                 display: "flex",
                 justifyContent: "space-between",
               }}
+              className="mb-4"
             >
-                <Typography
-                        sx={{ fontSize: 14 }}
-                        color="text.secondary"
-                        gutterBottom
-                      >
-                        Community Tips
-                      </Typography>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Your Tips
+              </Typography>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Total Tips: {userTipsData.length}
+              </Typography>
             </Box>
+            <Box sx={{ flexGrow: 1 }}>
+              <Grid container spacing={2}>
+                {displayedUserTips.map((userTips, tipIndex) => (
+                  <Grid item xs={3} key={tipIndex}>
+                    <div className="mb-4">
+                      <Item>
+                        <Typography variant="subtitle1" component="div">
+                          {userTips.university}
+                        </Typography>
+                        <Divider />
+                        <Typography variant="subtitle1" component="div">
+                          {userTips.message}
+                        </Typography>
+                        <Divider />
+                        <Typography variant="subtitle1" component="div">
+                          {userTips.name}
+                        </Typography>
+                      </Item>
+                    </div>
+                  </Grid>
+                ))}
+              </Grid>
+            </Box>
+            {!showAllTips && (
+              <Button onClick={handleToggleUserTips} variant="outlined" color="primary" sx={{ mt: 2 }}>
+                Show More Tips
+              </Button>
+            )}
           </CardContent>
         </Card>
       </Box>
@@ -73,4 +142,4 @@ const Item = styled(Paper)(({ theme }) => ({
   );
 };
 
-export default TipsDetailsCard
+export default TipsDetailsCard;
--- app/routes/communitytips._index.tsx
+++ app/routes/communitytips._index.tsx
@@ -3,46 +3,50 @@ import { Link, useLoaderData } from "@remix-run/react";
 import NavBar from "~/components/NavBar";
 import { requireUserId } from "~/session.server";
 import { getAllTips, getTipsByUserId } from "~/models/tips.server";
-import TipsDetailsCard from "../components/TipsDetailsCard"
+import TipsDetailsCard from "../components/TipsDetailsCard";
+import { Box, Grid } from "@mui/material";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const userId = await requireUserId(request);
 
-  const allTips = await getAllTips()
-  const userTips = await getTipsByUserId(userId)
+  const allTips = await getAllTips();
+  const userTips = await getTipsByUserId(userId);
 
-  return {allTips, userTips}
-}
+  return { allTips, userTips };
+};
 
 export default function CommunityTips() {
-
   const tipsData = useLoaderData<typeof loader>();
-  console.log(tipsData)
-  
+  console.log(tipsData);
+
   return (
     <div className="min-h-full">
       <NavBar />
       <header className="bg-white shadow">
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-            Welcome to Community Tips
-          </h1>
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex items-center justify-between">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">Welcome to Community Tips</h1>
+          <Link
+            to="/communitytips/addtip"
+            className="block rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400 text-center"
+          >
+            Add Community Tip
+          </Link>
         </div>
       </header>
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1>Here you will view and share Tips from other students! </h1>
+        <h2 className="text-lg font-semibold text-gray-700 px-4 py-2">
+          Here you will view and share Tips from other students!
+        </h2>
+        <div>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TipsDetailsCard tipsData={tipsData.allTips} userTipsData={tipsData.userTips} />
+              </Grid>
+            </Grid>
+          </Box>
+        </div>
       </div>
-
-      <Link
-        to="/communitytips/addtip"
-        className="block w-full rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400 text-center"
-      >
-        Add Community Tip
-      </Link>
-
-      <TipsDetailsCard tipsData={null} userTipsData={null} /> 
-
-      
     </div>
   );
 }
